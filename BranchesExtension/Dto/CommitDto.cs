35bb524e201603c2b997ca970e69e355fba290e2
pavelski01 <pavelski01@gmail.com>
2018-12-26 13:03:31 +0100

    Add data transfer objects

---
+++ BranchesExtension/Dto/CommitDto.cs
@@ -0,0 +1,15 @@
+﻿using Soneta.Types;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace enova365.Dodatek1.Dto
+{
+    public class CommitDto
+    {
+        public string Committer { get; set; }
+        public Date Date { get; set; }
+        public int CommitsNumber { get; set; }
+        public double CommitsPersonalAverage { get; set; }
+        public double CommitsProjectAverage { get; set; }
+    }
+}
---
+++ BranchesExtension/Dto/ProjectInfoDto.cs
@@ -0,0 +1,12 @@
+﻿using Soneta.Types;
+
+namespace enova365.Dodatek1.Dto
+{
+    public class ProjectInfoDto
+    {
+        public Date MinDate { get; set; }
+        public Date MaxDate { get; set; }
+        public int DaysActivity { get; set; }
+        public FromTo ProjectActivity { get; set; }
+    }
+}
